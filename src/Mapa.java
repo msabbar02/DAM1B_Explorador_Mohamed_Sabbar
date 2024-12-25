a3b68ffe008d79ae0fdb39c6dcdfc4e2af841747
msabbar02 <msabbar1@educacion.navarra.es>
2024-12-25 23:33:23 +0100

    los atributos de la clase Mapa y el metodo mostrar

--- src/Mapa.java
+++ src/Mapa.java
@@ -1,2 +1,71 @@
+import java.awt.*;
+import java.util.Random;
+
 public class Mapa {
+    // Atributos
+    private char [][] tablero;
+    private Posicion posTesoro;
+    private Posicion posJugador;
+    private Enemigo [] listadoEnemigos;
+    private Posicion [] posicionTramapas;
+
+    //Constructor
+    public Mapa(){
+
+        tablero = new char[6][20];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 20; j++) {
+                tablero[i][j] = ' ';
+            }
+        }
+
+        //tesoro
+        Random rand = new Random();
+        posTesoro = new Posicion(rand.nextInt(6),rand.nextInt(20));
+        tablero[posTesoro.getCoordenadaFila()][posTesoro.getCoordenadaCol()] = ' ';
+
+        // trampas
+        posicionTramapas = new Posicion[3];
+        for (int i = 0; i < 3; i++) {
+            Posicion trampa;
+            do {
+                trampa = new Posicion(rand.nextInt(6), rand.nextInt(20) );
+            }while (tablero[trampa.getCoordenadaFila()][trampa.getCoordenadaCol()] != ' ');
+            posicionTramapas[i] = trampa;
+            tablero[trampa.getCoordenadaFila()][trampa.getCoordenadaCol()] = 'T';
+        }
+
+        // Enemigo
+        listadoEnemigos = new Enemigo[3];
+        for (int i = 0; i < 3; i++) {
+            Enemigo enemigo;
+            do {
+                enemigo = new Enemigo();
+            }while (tablero[enemigo.getPosicionActual().getCoordenadaFila()][enemigo.getPosicionActual().getCoordenadaCol()] != ' ');
+            listadoEnemigos[i] = enemigo;
+            tablero[enemigo.getPosicionActual().getCoordenadaFila()][enemigo.getPosicionActual().getCoordenadaCol()] = 'E';
+        }
+    }
+    // Getters Y Setters
+    public char[][] getTablero() {
+        return tablero;
+    }
+
+    public Posicion getPosTesoro() {
+        return posTesoro;
+    }
+
+    // metodo Mostrar
+
+    public void mostrar(){
+        for (int i = 0; i < 6; i++) {
+            System.out.println("----------------------------------------------------------------------------------");
+            for (int j = 0; j < 20; j++) {
+                System.out.println("| "+ tablero[i][j] + " |");
+            }
+            System.out.println("----------------------------------------------------------------------------------");
+        }
+    }
+
+
 }
